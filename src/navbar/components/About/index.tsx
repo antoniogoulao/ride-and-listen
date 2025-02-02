524bc6b51469f56b2c199210ea54f0ac9667e066
Antonio Goulao <antoniomgoulao@gmail.com>
2025-02-02 14:56:38 +0000

    replace Twitter hiperlink by the bluesky hiperlink. add prettier.

--- src/navbar/components/About/index.tsx
+++ src/navbar/components/About/index.tsx
@@ -1,49 +1,45 @@
-import { Box, Typography, Link } from "@mui/material";
-import { GitHub, Twitter, Mail, Language } from "@mui/icons-material";
+import { Box, Link, Typography } from '@mui/material';
+import { GitHub, Language, Mail } from '@mui/icons-material';
+import { Bluesky } from '../../../svg/Bluesky';
 
 export const About = () => {
   return (
     <Box
       sx={{
-        display: "flex",
-        alignItems: "center",
-        flexDirection: "column",
-        minWidth: "max-content",
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        minWidth: 'max-content',
         marginX: 2,
-      }}
-    >
+      }}>
       <Typography>António Goulão</Typography>
       <Box>
         <Link
           marginX={0.25}
           aria-label="project github repo"
           target="blank"
-          href="https://github.com/antoniogoulao/ride-and-listen"
-        >
+          href="https://github.com/antoniogoulao/ride-and-listen">
           <GitHub color="secondary" />
         </Link>
         <Link
           marginX={0.25}
-          aria-label="twitter account"
+          aria-label="bluesky account"
           target="blank"
-          href="https://twitter.com/toniomg"
-        >
-          <Twitter color="secondary" />
+          href="https://bsky.app/profile/antoniogoulao.dev">
+          <Bluesky color="secondary" />
         </Link>
         <Link
           marginX={0.25}
           aria-label="email address"
           target="blank"
-          href="mailto:antoniomgoulao@gmail.com"
-        >
+          href="mailto:antoniomgoulao@gmail.com">
           <Mail color="secondary" />
         </Link>
         <Link
           marginX={0.25}
           aria-label="personal website"
           target="blank"
-          href="https://antoniogoulao.dev"
-        >
+          href="https://antoniogoulao.dev">
           <Language color="secondary" />
         </Link>
       </Box>
