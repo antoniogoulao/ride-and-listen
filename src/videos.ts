052df365e156ad7f3dd5e0c25f0e2d215b70158c
Antonio Goulao <antoniomgoulao@gmail.com>
2025-03-31 11:16:54 +0100

    add N118 - salvaterra - santarem to videos

--- src/videos.ts
+++ src/videos.ts
@@ -1,52 +1,56 @@
-import { Video } from "./types";
+import { Video } from './types';
 
 export const VIDEOS: Video[] = [
   {
-    name: "Portagem (Marvão) -> Castelo de Vide, Alentejo",
-    videoId: "rFshlSZQ7cU",
+    name: 'Portagem (Marvão) -> Castelo de Vide, Alentejo',
+    videoId: 'rFshlSZQ7cU',
   },
   {
-    name: "Glória -> Santarém, Ribatejo",
-    videoId: "wcFrsemxPu8",
+    name: 'Glória -> Santarém, Ribatejo',
+    videoId: 'wcFrsemxPu8',
   },
   {
-    name: "Caldas da Rainha -> Ferraria de São João, Centro",
-    videoId: "HQpDZfXMIu4",
+    name: 'Caldas da Rainha -> Ferraria de São João, Centro',
+    videoId: 'HQpDZfXMIu4',
   },
   {
-    name: "Setúbal -> Arrábida -> Sesimbra, AML",
-    videoId: "B58KZ1koOOg",
+    name: 'Setúbal -> Arrábida -> Sesimbra, AML',
+    videoId: 'B58KZ1koOOg',
   },
   {
     name: "Serra D'Arga, Minho",
-    videoId: "p_TaRrCR1O0",
+    videoId: 'p_TaRrCR1O0',
   },
   {
-    name: "Sierra de las Nieves, Espanha",
-    videoId: "0rwRDGOXShM",
+    name: 'Sierra de las Nieves, Espanha',
+    videoId: '0rwRDGOXShM',
   },
   {
-    name: "Subida da Serra de Montejunto, Estremadura",
-    videoId: "Czlcw_A3anQ",
+    name: 'Subida da Serra de Montejunto, Estremadura',
+    videoId: 'Czlcw_A3anQ',
   },
   {
-    name: "Passeio Festas da Glória 2023, Ribatejo",
-    videoId: "gGwBYDZUVqM",
+    name: 'Passeio Festas da Glória 2023, Ribatejo',
+    videoId: 'gGwBYDZUVqM',
   },
   {
-    name: "N621 até Riaño, Espanha",
-    videoId: "RyqlEspPze8",
+    name: 'N621 até Riaño, Espanha',
+    videoId: 'RyqlEspPze8',
   },
   {
-    name: "Passeio da Liberdade 2024, Ribatejo",
-    videoId: "WsZmnbmZmxw",
+    name: 'Passeio da Liberdade 2024, Ribatejo',
+    videoId: 'WsZmnbmZmxw',
   },
   {
-    name: "Muge -> Cartaxo -> Santarém, Ribatejo",
-    videoId: "42Lu81wmQOY"
+    name: 'Muge -> Cartaxo -> Santarém, Ribatejo',
+    videoId: '42Lu81wmQOY',
   },
   {
-    name: "Fátima -> Alcanena, Ribatejo",
-    videoId: "C3019-dH1KA"
-  }
+    name: 'Fátima -> Alcanena, Ribatejo',
+    videoId: 'C3019-dH1KA',
+  },
+  {
+    name: 'N118 -> Salvaterra de Magos -> Santarém, Ribatejo',
+    videoId: 'JHtkB4itAQ0',
+  },
 ];
